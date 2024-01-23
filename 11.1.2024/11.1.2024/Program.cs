d043cac01c886a5679453aef120bc14338f7063b
Aykut5647 <157456537+Aykut5647@users.noreply.github.com>
2024-01-23 09:39:11 +0200

    Add files via upload

---
+++ 11.1.2024/11.1.2024/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._1._2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double age = int.Parse(Console.ReadLine());
+            string pol = Console.ReadLine();
+            if (age < 16)
+            {
+
+                if (pol == "M") Console.WriteLine("Master");
+                else if (pol == "f") Console.WriteLine("Miss");
+            }
+            else
+            {
+                if (pol == "M") Console.WriteLine("Mr.");
+                else if (pol == "f") Console.WriteLine("Ms.");
+            }
+        }
+    }
+}
