d9134c7eb6f85aa0811c47d0a085990252d2ba76
Aykut5647 <157456537+Aykut5647@users.noreply.github.com>
2024-01-23 09:33:49 +0200

    Add files via upload

---
+++ 16.01.2024/16.01.2024/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16._01._2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vivedi si imeto");
+            string name = (Console.ReadLine());
+            Console.WriteLine("Hi," + name);
+            string str = " Hello, 9a";
+            string str1 = new string(new char[] { 'O','h',','});
+            char[] charArr = str.ToCharArray();
+            string str2 = string.Concat(str1, str);
+            string str3= new string(charArr);
+            Console.WriteLine(str2);
+        }
+    }
+}
---
+++ 18.1.2024/18.1.2024/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace _18._1._2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string listOfBeers = "Amstel, Zagorka, Tuborg, Becks.";
+            string[] beers = listOfBeers .Split(' ', ',', '.');
+            Console.WriteLine("Available beers are:");
+            foreach (string beer in beers)
+                Console.WriteLine(beer);
+            string Защо = "Az,Aykut,obicham,da,piq,voda";
+            string[] защото = Защо.Split(' ', ',', '.');
+            Console.WriteLine("Завирших, защото:");
+            foreach (string Aykut in защото)
+                Console.WriteLine(Aykut);
+            string replaced = listOfBeers.Replace(",", " ");
+            //Vodka and Martini and Cherry
+            Console.WriteLine(replaced);
+            string novo = replaced.Remove(8, 8);
+            Console.WriteLine(novo);
+            string lowerAlpha = novo.ToLower();
+            Console.WriteLine(lowerAlpha);
+            string upperAlpha = novo.ToUpper();
+            Console.WriteLine(upperAlpha);
+            
+            string novo1 = Защо.Replace("voda", "maze");
+            string novo2 = Защо.Remove(24, 4);
+            Console.WriteLine(novo2);
+            string lowerAlpha1 = Защо.ToLower();
+            Console.WriteLine(lowerAlpha1);
+            string upperAlpha2 = Защо.ToUpper();
+            Console.WriteLine(upperAlpha2);
+        }   
+    }
+}
